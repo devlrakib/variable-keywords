6add86e0ba9ea8d5d7fec0c3b338be040dc6561f
devlrakib <devrakib5@gmail.com>
2024-03-05 14:54:19 +0600

    first commit

---
+++ variable-name.js
@@ -0,0 +1,14 @@
+var price=29;
+// 01. Variable Name cannot be any reserved keywords in javascript
+var false=50;
+// 02. Variable Name has to be in one work.No Space
+var my home address ="california";
+
+//03. Variable Name Cannot Have Quation
+var "home" ="pangsha";
+
+//04. Variable Name Cannot Starts With A Number but can ends with a number
+var 99home ="hgg";
+var home99 ="abcd";
+
+//05. How To USe Long name
